08c34eae41d618def9eaaff0bca0613b502cefb9
Tobias Widlund <widlundtobias@gmail.com>
2013-05-19 21:17:31 +0200

    competed documentation for event.h

--- include/framework/input/event.h
+++ include/framework/input/event.h
@@ -119,21 +119,114 @@ namespace windbreeze
      *  @var Event::type
      *  @brief Type of the event.
      *
-     *  @var Event::size
+     ***********FIX
+     *  @var Event::Data::size
      *  @brief Holds data of RESIZED events.
      *
-     *  @var Event::key
+     *  @var key
      *  @brief Holds data of KEYPRESSED and KEYRELEASED events.
-     * 
+     **************ENDFIX
      *
      *
      *  @struct Event::ResizeEvent
-     *  @brief Data structure to contain size information for resizing events.
+     *  @brief Data structure containing data for resize events.
      *
      *  @var Event::ResizeEvent::width
      *  @brief width of new size.
      *
      *  @var Event::ResizeEvent::height
      *  @brief height of new size.
+     *
+     *
+     *  @struct Event::KeyEvent
+     *  @brief Data structure containing data for key events.
+     *
+     *  @var Event::KeyEvent::code
+     *  @brief Numerical code corresponing to a key on the keyboard.
+     *
+     *  @var Event::KeyEvent::alt
+     *  @brief True if alt was held down while the event was fired.
+     *
+     *  @var Event::KeyEvent::control
+     *  @brief True if control was held down while the event was fired.
+     *
+     *  @var Event::KeyEvent::shift
+     *  @brief True if shift was held down while the event was fired.
+     *
+     *  @var Event::KeyEvent::system
+     *  @brief True if the system key was held down while the event was fired.
+     *
+     *
+     *  @struct Event::TextEvent
+     *  @brief Data structure containing data for text events.
+     *
+     *  @var Event::TextEvent::unicode
+     *  @brief Integer representing unicode value.
+     *
+     *  
+     *  @struct MouseMoveEvent
+     *  @brief Data structure containing data for mouse move events.
+     *
+     *  @var Event::MouseMoveEvent::x
+     *  @brief X coordinate.
+     *
+     *  @var Event::MouseMoveEvent::y
+     *  @brief Y coordinate.
+     *
+     *
+     *  @struct MouseButtonEvent
+     *  @brief Data structure containing data for mouse button events.
+     *
+     *  @var Event::MouseButtonEvent::button
+     *  @brief Numerical code representing the mouse button.
+     *
+     *  @var Event::MouseButtonEvent::x
+     *  @brief X coordinate.
+     *
+     *  @var Event::MouseButtonEvent::y
+     *  @brief Y coordinate.
+     *
+     *  
+     *  @struct MouseWheelEvent
+     *  @brief Data structure containing data for mouse wheel events.
+     *
+     *  @var Event::MouseWheelEvent::delta
+     *  @brief Numerical value representing how much the mouse wheel was moved. Backward move will yield a negative number.
+     *
+     *  @var Event::MouseWheelEvent::x
+     *  @brief X coordinate.
+     *
+     *  @var Event::MouseWheelEvent::y
+     *  @brief Y coordinate.
+     *
+     *  
+     *  @struct Event::GamepadConnectEvent
+     *  @brief Data structure containing data for gamepad connect events.
+     *
+     *  @var Event::GamepadConnectEvent::gamepadId
+     *  @brief Numerical ID representing the gamepad which was connected.
+     *
+     *  
+     *  @struct Event::GamepadMoveEvent
+     *  @brief Data structure containing data for gamepad move events.
+     *
+     *  @var Event::GamepadMoveEvent::gamepadId
+     *  @brief Numerical ID representing the gamepad for which the axis was moved.
+     *
+     *  @var Event::GamepadMoveEvent::axis
+     *  @brief Numerical code representing moved axis.
+     *
+     *  @var Event::GamepadMoveEvent::position
+     *  @brief Decimal number of the new position of the axis.
+     *
+     *  
+     *  @struct Event::GamepadButtonEvent
+     *  @brief Data structure containing data for gamepad button events.
+     *
+     *  @var Event::GamepadButtonEvent::gamepadId
+     *  @brief Numerical ID representing the gamepad for which the button even was fired.
+     *
+     *  @var Event::GamepadButtonEvent::button
+     *  @brief Button code representing the button which the event belongs to.
      */
 }
