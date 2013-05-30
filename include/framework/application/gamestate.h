9d86228808b51672f27bc503b1c6a36ffe34b75b
Tobias Widlund <widlundtobias@gmail.com>
2013-05-30 19:10:31 +0200

    done with gamestate documentation

--- include/framework/application/gamestate.h
+++ include/framework/application/gamestate.h
@@ -42,8 +42,29 @@ namespace windstorm
      *  @fn virtual void GameState::handOver(std::weak_ptr<GameState> previousState, std::string previousStateName)
      *  @brief Hand over data from another GameState instance to this one.
      *  
-     *  This function is called by the GameStateMachine when it switches from this state to another state. When it is called, both this and the other GameState instance is in a deactivated state. The purpose of this function is to let this state fetch data from the previous GameState if needed. For example, if this GameState defines a high score state, it could fetch the final game score from the previous in-game state.
+     *  This function is called by the GameStateMachine when it switches from this state to another state. When it is called, both this and the other GameState instance will be in a deactivated state. The purpose of this function is to let this state fetch data from the previous GameState if needed. For example, if this GameState defines a high score state, it could fetch the final game score from the previous in-game state.
      *  @param previousState Pointer to the previous GameState.
      *  @param previousStateName Name of the previous GameState.
+     ***
+     *  @fn virtual void GameState::setup()
+     *  @brief Setup a game state.
+     *  
+     *  This function is called by the GameStateMachine when the GameState is added to it. Hence this function should perform a one-time setup of the GameState if needed.
+     ***
+     *  @fn virtual std::string GameState::run() = 0
+     *  @brief Perform per-frame logic of the state.
+     *
+     *  This function is called by the GameStateMachine every frame when this GameState is the current active state. This is where the logic of the state goes.
+     *  @return Returns the name of the next GameState to switch to. If the string is not empty, the GameStateMachine will switch to that state if it exists. If "NONE" is returned, the GameStateMachine will deactivate the running state and be halted.
+     ***
+     *  @fn virtual void GameState::destroy()
+     *  @brief Destroy a game state.
+     *  
+     *  This function is called by the GameStateMachine when it is destroyed. It will only ever be called once per GameState added to the GameStateMachine. This function is for freeing memory or clearing up other things that need it.
+     ***
+     *  @fn virtual GameState::~GameState()
+     *  @brief Virtual destructor.
+     * 
+     *  This destructor is virtual to let the derived classes create their own destructors if they need to.
      ***/
 }
