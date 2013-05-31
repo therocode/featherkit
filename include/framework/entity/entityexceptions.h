e1cb6b3118aa32247fa69aa6e1db68bd2ff9e1ba
Tobias Widlund <widlundtobias@gmail.com>
2013-06-01 01:10:51 +0200

    Wrote documentation for entityexceptions.h

--- include/framework/entity/entityexceptions.h
+++ include/framework/entity/entityexceptions.h
@@ -19,4 +19,24 @@ namespace windgale
                 : std::runtime_error(m), hash(h) { };
             AttributeHash hash;
     };
+    /** @addtogroup EntitySystem
+     *@{
+     *  @class InvalidIdException
+     *  
+     *  @class InvalidAttributeException
+     *@}
+     ***
+     *  @class InvalidIdException
+     *  @brief Exception used by the Entity System when something related to EntityId handling goes wrong.
+     *
+     *  @fn InvalidIdException::InvalidIdException(const std::string& message)
+     *  @brief Construct an exception to throw containing a message.
+     *  @param message Message further describing the error.
+     ***
+     *  @class InvalidAttributeException
+     *  @brief Exception used by the Entity System when something related to attribute handling goes wrong.
+     *
+     *  @fn InvalidAttributeException::InvalidAttributeException
+     *  @param message Message further describing the error.
+     ***/
 }
