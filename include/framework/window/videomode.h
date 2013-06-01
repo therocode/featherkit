a25ca7466ed16fec69c8d18c2b51d636485a0b77
kimspindel <kimspindel@gmail.com>
2013-06-01 15:24:36 +0200

    docs for videomode

--- include/framework/window/videomode.h
+++ include/framework/window/videomode.h
@@ -3,9 +3,8 @@
 
 namespace windbreeze
 {
-    class VideoMode
+    struct VideoMode
     {
-        public:
             VideoMode() {}
             VideoMode(uint32_t w, uint32_t h, uint32_t bitDepth=32) : width(w), height(h), bitsPerPixel(bitDepth) {}
 
@@ -13,5 +12,31 @@ namespace windbreeze
             uint32_t height;
             uint32_t bitsPerPixel;
     };
+
+    /** @addtogroup UserInterface
+     *@{
+     *  @struct VideoMode
+     *@}
+     ***
+     *  @struct VideoMode
+     *  @brief Defines a video mode used to set up windows at creation time.
+     *
+     ***
+     *  @fn VideoMode::VideoMode(uint32_t w, uint32_t h, uint32_t bitDepth=32) 
+     *  @brief Default constructor.
+     *
+     *  @param w Width in pixels.
+     *  @param h Width in pixels.
+     *  @param bitsPerPixel Pixel depth in bits per pixel.
+     ***
+     *  @var VideoMode::width
+     *  @brief The width of the video mode in pixels.
+     ***
+     *  @var VideoMode::height
+     *  @brief The height of the video mode in pixels.
+     ***
+     *  @var VideoMode::bitsPerPixel
+     *  @brief The depth of the video mode in bits per pixels.
+     ***/
 
 }
