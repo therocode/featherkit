6820f3ac992075a243cfeb6ee1b8809e9cc9b849
Tobias Widlund <widlundtobias@gmail.com>
2013-06-02 20:02:00 +0200

    documented jsonactioniohandler.h

--- include/framework/util/input/jsonactioniohandler.h
+++ include/framework/util/input/jsonactioniohandler.h
@@ -36,6 +36,13 @@ namespace windbreeze
 
         std::ofstream file(path);
 
+        if(!file)
+        {
+            std::stringstream ss;
+            ss << "Error! Action bindings file not found: " << path << "\n";
+            throw windgale::FileNotFoundException(ss.str());
+        }
+
         for(auto binding : primaryActions)
         {
             ActionTrigger::ActionType type = binding.first.type;
@@ -142,7 +149,7 @@ namespace windbreeze
 
         json::write(file, root);
     }
-    /** @addtogroup EntitySystem
+    /** @addtogroup UserInterface
      *@{
      *  @class JsonActionIOHandler
      *@}
@@ -151,58 +158,36 @@ namespace windbreeze
      *  @brief This class is for loading and saving json files describing Action bindings.
      *
      *  The output of the loading functions of this class can directly be passed to the ActionHandler to bind the actions saved. To bind the actions, first load a json file using JsonActionIOHandler::loadBindingsFile. Then pass the output of JsonActionIOHandler::getPrimaryBindings into ActionHandler::setPrimaryBindings and JsonActionIOHandler::getSecondaryBindings into ActionHandler::setSecondaryBindings if needed.
-     *
-     *  Here is an example of how an attribute file should be formatted:
-     *  @code
-     *  {
-     *      "position":"8",
-     *      "velocity":"8",
-     *      "originalcolour":"12",
-     *      "currentcolour":"12",
-     *      "spawnrate":"#uint32#",
-     *      "spawnpower":"#float#",
-     *      "deathrate":"#uint32#",
-     *      "id":"#uint32#"
-     *  }
-     *  @endcode
-     *  The formatting is very simple. Every row defines an attribute that could be used in an Entity type definition. The two values needed per row are firstly the name of the attribute, and secondly the amount of memory needed to store it in bytes. For example, the first row defines an attribute called "position". Since the position in the application will be implemented using a struct composed of two floats, one for X and one for Y, and floats are 4 bytes each, a total of 8 bytes are needed.
-     *
-     *  It is also possible to have special data type tags instead of numbers. These tags directly correspond to sizes of C++ data types. Here is a complete list of data type tags and the C++ types they correspond to.
-     *  - \#uint32\# - uint32_t
-     *  - \#int32\# - int32_t
-     *  - \#float\# - float
-     *  - \#double\# - double
-     *  - \#bool\# - bool
-     *  - \#byte\# - char
-     *  - \#char\# - char
-     *
-     *  Here is an example of how an Entity type file should be formatted:
-     *  @code
-     *  {
-     *      "particle":
-     *      {
-     *          "position":"20.0f,40.0f", "velocity":"0.0f,0.0f", "currentcolour":"1.0f, 0.0f, 0.0f", "deathrate":"9000", "id":"#ID#"
-     *      },
-     *      "spawner":
-     *      {
-     *          "position":"30.0f,50.0f", "currentcolour":"", "spawnrate":"100", "spawnpower":"5.0f", "id":"#ID#"
-     *      }
-     *  }
-     *  @endcode
-     *  The file is divided into Entity type definitions. The two entities defined in this files are "particle" and "spawner". Every Entity type internally defines a sequence of attributes that they should have. The attributes might come with default values. For instance, the first attribute of the Entity type "particle" is "position" with a default value of "20.0f,40.0".
      ***
-     *  @fn std::map<std::string, int> JsonEntityLoader::loadAttributesJson(const std::string& path)
-     *  @brief Load a json file defining Entity attributes.
+     *  @fn void JsonActionIOHandler::loadBindingsFile(const std::string& path) const
+     *  @brief Load a json file defining bindings for actions.
      *
-     *  Use this function to load an attribute file. The return value can be given to the EntityManager::loadAttributes function.
+     *  Use this function to load an attribute file. When the bindings are loaded, they can be accessed with JsonActionIOHandler::getPrimaryBindings and JsonActionIOHandler::getSecondaryBindings.
      *  @param path File to open.
-     *  @return A map with the attributes, to pass to the EntityManager.
      ***
-     *  @fn std::map<std::string, std::map<std::string, std::string> > JsonEntityLoader::loadEntitiesJson(const std::string& path)
-     *  @brief Load a json file defining Entity types.
+     *  @fn void JsonActionIOHandler::saveBindingsFile(const std::string& path, const std::map<ActionTrigger, Action>& primaryActions) const
+     *  @brief Save action bindings to a json formatted file.
+     *  
+     *  @param path File to save the bindings in.
+     *  @param primaryActions Map of primary actions. Obtainable through the ActionHandler.
+     ***
+     *  @fn void JsonActionIOHandler::saveBindingsFile(const std::string& path, const std::map<ActionTrigger, Action>& primaryActions, const std::map<ActionTrigger, Action>& secondaryActions) const
+     *  @brief Save action bindings to a json formatted file.
      *
-     *  Use this function to load an Entity type file. The return value can be given to the EntityManager::loadEntityTypes function.
-     *  @param path File to open.
-     *  @return A map with the Entity types, to pass to the EntityManager.
+     *  @param path File to save the bindings in.
+     *  @param primaryActions Map of primary actions. Obtainable through the ActionHandler.
+     *  @param secondaryActions Map of secondary actions. Obtainable through the ActionHandler.
+     ***
+     *  @fn const std::map<ActionTrigger, Action> JsonActionIOHandler::getPrimaryBindings() const
+     *  @brief Retrieve loaded primary bindings.
+     *
+     *  JsonActionIOHandler::loadBindingsFile must be called before this function or the returned map will be empty.
+     *  @return Map containing primary bindings.
+     ***
+     *  @fn const std::map<ActionTrigger, Action> JsonActionIOHandler::getSecondaryBindings() const
+     *  @brief Retrieve loaded secondary bindings.
+     *
+     *  JsonActionIOHandler::loadBindingsFile must be called before this function or the returned map will be empty.
+     *  @return Map containing secondary bindings.
      **/
 }
