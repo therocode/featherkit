364ff911c2b49b4b7f45f72e8607121b476ecc06
Tobias Widlund <widlundtobias@gmail.com>
2013-06-16 21:49:35 +0200

    added animation file

---
+++ include/framework/rendering/animation.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace windbreeze
+{
+    //Animation
+    //
+    //Start x, y   in % of texture
+    //FrameSize w, h  in % of texture
+    //FrameAmount n  amount of frames in animation
+    //Delay  t    time in frames between animation frames
+    //Loop  should it loop or not
+    //Behaviour  Forwards, Backwards, Bounce, ReverseBounce
+}
