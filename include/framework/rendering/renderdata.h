e97d587fcfb8052785d4566dc09f869fdedc7201
Tobias Widlund <widlundtobias@gmail.com>
2013-06-16 16:29:52 +0200

    texture wrapping using constrained areas working

--- include/framework/rendering/renderdata.h
+++ include/framework/rendering/renderdata.h
@@ -8,5 +8,7 @@ namespace windbreeze
         std::vector<float> vertices;
         std::string textureId;
         std::vector<float> texCoords;
+        glm::vec2 constrainX = glm::vec2(1.0f, 0.0f);
+        glm::vec2 constrainY = glm::vec2(1.0f, 0.0f);
     };
 }
--- src/util/rendering/sfml2dbackend.cpp
+++ src/util/rendering/sfml2dbackend.cpp
@@ -19,31 +19,52 @@ namespace windbreeze
         //vertex = rotation * (viewport.getCamera().getZoom() * (vertex - viewport.getCamera().getPosition())) + halfViewSize
 
         std::string vertexShaderSource = "#version 120\n"
-                                         "uniform vec2 position;"
-                                         "uniform vec2 zoom;"
-                                         "uniform mat2 rotation;"
-                                         "uniform vec2 halfViewSize;"
-                                         ""
-                                         "varying vec2 texCoords;"
-                                         ""
-                                         "void main()"
-                                         "{"
-                                         "    vec2 transformedPoint = rotation * (zoom * (gl_Vertex.xy - position)) + halfViewSize;"
-                                         "    gl_Position = vec4(transformedPoint.xy, gl_Vertex.zw);"
-                                         "    gl_Position = gl_ProjectionMatrix * (gl_Position);"
-                                         "    gl_TexCoord[0]  = gl_MultiTexCoord0;"
-                                         "}"
+                                         "\n"
+                                         "uniform vec2 position;\n"
+                                         "uniform vec2 zoom;\n"
+                                         "uniform mat2 rotation;\n"
+                                         "uniform vec2 halfViewSize;\n"
+                                         "\n"
+                                         "void main()\n"
+                                         "{\n"
+                                         "    vec2 transformedPoint = rotation * (zoom * (gl_Vertex.xy - position)) + halfViewSize;\n"
+                                         "    gl_Position = vec4(transformedPoint.xy, gl_Vertex.zw);\n"
+                                         "    gl_Position = gl_ProjectionMatrix * (gl_Position);\n"
+                                         "    gl_TexCoord[0]  = gl_MultiTexCoord0;\n"
+                                         "}\n"
                                          "";
 
         std::string fragmentShaderSource = "#version 120\n"
-                                           "uniform sampler2D texture;"
-                                           ""
-                                           "varying vec2 texCoords;"
-                                           ""
-                                           "void main()"
-                                           "{"
-                                           "    gl_FragColor = texture2D(texture, gl_TexCoord[0].st);"
-                                           "}"
+                                           "uniform sampler2D texture;\n"
+                                           "uniform vec2 constrainX;\n"
+                                           "uniform vec2 constrainY;\n"
+                                           "\n"
+                                           "float boundBetween(float val, float lowerBound, float upperBound)\n"
+                                           "{\n"
+                                           "    if(lowerBound > upperBound)\n"
+                                           "    {\n"
+                                           "        float temp = lowerBound;\n"
+                                           "        lowerBound = upperBound;\n"
+                                           "        upperBound = temp;\n"
+                                           "    }\n"
+                                           "    \n"
+                                           "    val = val - lowerBound;\n"
+                                           "    float rangeSize = upperBound - lowerBound;\n"
+                                           "    if(rangeSize == 0)\n"
+                                           "    {\n"
+                                           "        return upperBound;\n"
+                                           "    }\n"
+                                           "    return val - (rangeSize * floor(val/rangeSize)) + lowerBound;\n"
+                                           "}\n"
+                                           "\n"
+                                           "void main()\n"
+                                           "{\n"
+                                           "\n"
+                                           "    vec2 constraintSize = abs(vec2(constrainX[1] - constrainX[0] , constrainY[1] - constrainY[0]));\n"
+                                           "    vec2 texCoords = constraintSize * gl_TexCoord[0].st + vec2(constrainX[0], constrainY[0]);\n"
+                                           "    texCoords = vec2(boundBetween(texCoords.s, constrainX[0], constrainX[1]), boundBetween(texCoords.t, constrainY[0], constrainY[1]));\n"
+                                           "    gl_FragColor = texture2D(texture, texCoords);\n"
+                                           "}\n"
                                            "";
         
         ShaderLoader loader;
@@ -95,6 +116,18 @@ namespace windbreeze
         {
             OpenGLTexture texture = textureManager.getTexture(renderData.textureId);
             glBindTexture(GL_TEXTURE_2D, texture.glId);
+
+            //glm::ivec2 size;
+            //glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &size[0]);
+            //glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &size[1]);
+
+            //glm::ivec2 gT
+
+            GLint constrainXUniform = glGetUniformLocation(shaderProgram, "constrainX");
+            glUniform2fv(constrainXUniform, 1, glm::value_ptr(renderData.constrainX));
+
+            GLint constrainYUniform = glGetUniformLocation(shaderProgram, "constrainY");
+            glUniform2fv(constrainYUniform, 1, glm::value_ptr(renderData.constrainY));
         }
         else
         {
@@ -112,8 +145,4 @@ namespace windbreeze
         glDisableClientState(GL_VERTEX_ARRAY);
         glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     }
-
-    //Good morning!
-    //
-    //Fix wrapping and frames for repeated thingy, then implement parallax!
 }
