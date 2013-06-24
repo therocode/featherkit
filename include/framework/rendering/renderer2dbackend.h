91f5eba1497cf513a6178a425b2d3dab19e86e5e
Tobias Widlund <widlundtobias@gmail.com>
2013-06-24 19:41:48 +0200

    documentationisboringandmonotonousHEJ

--- include/framework/rendering/renderer2dbackend.h
+++ include/framework/rendering/renderer2dbackend.h
@@ -21,4 +21,58 @@ namespace windbreeze
         protected:
             Viewport viewport;
     };
+    /** @addtogroup Render2D
+     *@{
+     *  @class Renderer2DBackend
+     *@}
+     ***
+     *  @class Renderer2DBackend
+     *  @brief Base class for 2D rendering implementations.
+     *
+     *  The Renderer2D class relies on an imlpementation of the Renderer2DBackend class to render, and everything rendered will be rendered by the backend.
+     *
+     *  The rendering is handled by a backend to offer a way to select how the rendering is done. Different backends may implement it differently. For instance, there may be an OpenGL backend or a Direct3D backend.
+     ***
+     *  @fn virtual void Renderer2DBackend::setup() = 0
+     *  @brief Setup the backend.
+     *
+     *  This function is called by the Renderer2D::setup function and the implementation should contain all one-time setup procedures needed before rendering is started.
+     ***
+     *  @fn virtual void Renderer2DBackend::destroy() = 0
+     *  @brief Destroy the backend.
+     *  
+     *  This function is called by the Renderer2D::destroy function and the implementation should contain the destruction of resources acquired within the Renderer2DBackend::setup function.
+     ***
+     *  @fn virtual void Renderer2DBackend::clear() = 0
+     *  @brief Clear what has been drawn to the screen.
+     *  
+     *  This function is called by the Renderer2D::clear function and should clear the screen.
+     ***
+     *  @fn virtual void Renderer2DBackend::preRender() = 0
+     *  @brief Prepare the renderer for the next frame.
+     *  
+     *  This function is called by the Renderer2D::preRender function typically once every frame before anything is rendered. The implementation should contain everything needed to prepare the renderer for the upcoming frame.
+     ***
+     *  @fn virtual void Renderer2DBackend::render(RenderData renderData) = 0
+     *  @brief Render an object.
+     *
+     *  This function is called by the Renderer2D class when something needs to be rendered. The RenderData argument contains all needed information to render the object. The object drawn should be transformed accordingly with the Camera.
+     *  @param renderData Information on the object to be rendered.
+     ***
+     *  @fn virtual void Renderer2DBackend::postRender() = 0
+     *  @brief Finish the current frame's rendering.
+     *
+     *  This function is called by the Renderer2D::postRender function typically at the end of every frame, after all things are rendered. The implementation should contain everything needed to finalise the rendering.
+     ***
+     *  @fn void Renderer2DBackend::setViewport(const Viewport& v)
+     *  @brief Set the Viewport.
+     *  
+     *  This function is automatically called by the Renderer2D::setViewport function.
+     *  @param v Viewport to set.
+     ***
+     *  @fn virtual void renderText(const TextData& textData) = 0
+     *  @brief Render a text object to the screen.
+     *  
+     * 
+     ***/
 }
--- include/framework/rendering/viewport.h
+++ include/framework/rendering/viewport.h
@@ -11,11 +11,11 @@ namespace windbreeze
             Viewport(uint32_t w, uint32_t h) : size(glm::uvec2(w, h)) {}
             Viewport(uint32_t w, uint32_t h, const Camera& cam) : size(glm::uvec2(w, h)), camera(cam) {}
             void setSize(const glm::uvec2& s);
-            glm::uvec2& getSize();
+            glm::uvec2& getSize() const;
             void setCamera(const Camera& cam);
-            Camera& getCamera();
-            glm::vec2 transformPoint(const glm::vec2 point);
-            glm::vec2 untransformPoint(const glm::vec2 point);
+            Camera& getCamera() const;
+            glm::vec2 transformPoint(const glm::vec2 point) const;
+            glm::vec2 untransformPoint(const glm::vec2 point) const;
         private:
             glm::uvec2 size;
             Camera camera;
@@ -52,7 +52,7 @@ namespace windbreeze
      *  @brief Set the size of the Viewport.
      *  @param s Size to set it to.
      ***
-     *  @fn glm::uvec2& Viewport::getSize()
+     *  @fn glm::uvec2& Viewport::getSize() const
      *  @brief Get the size of the Viewport.
      *  @return Vector containing the current size.
      ***
@@ -60,18 +60,18 @@ namespace windbreeze
      *  @brief Set the Camera of the Viewport.
      *  @param cam Camera to set it to.
      ***
-     *  @fn Camera& Viewport::getCamera()
+     *  @fn Camera& Viewport::getCamera() const
      *  @brief Get the Camera currently in use by the Viewport.
      *  @return The current camera.
      ***
-     *  @fn glm::vec2 Viewport::transformPoint(const glm::vec2 point)
+     *  @fn glm::vec2 Viewport::transformPoint(const glm::vec2 point) const
      *  @brief Transform a point to the Viewport.
      *
      *  The point given will be transformed into the coordinate space of the Viewport. For instance, it can be used to calculate where on the screen a certain object will be rendered if this Viewport is used.
      *  @param point World space coordinate to transform.
      *  @return The given point in the space of this Viewport.
      ***
-     *  @fn glm::vec2 Viewport::untransformPoint(const glm::vec2 point)
+     *  @fn glm::vec2 Viewport::untransformPoint(const glm::vec2 point) const
      *  @brief Transform a point from the Viewport back to world coordinates.
      *
      *  Does a reverse Viewport transformation of the given point. The point is given in screen space, and the return value will represent the given point in world coordinates. For example, this can be used to decide where in the world coordinates a given screen point is.
