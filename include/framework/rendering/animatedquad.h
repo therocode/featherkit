763802d49edb8f289026a99dfa8ff8e129137979
Tobias Widlund <widlundtobias@gmail.com>
2013-06-25 15:46:51 +0200

    added some documentation

--- include/framework/rendering/animatedquad.h
+++ include/framework/rendering/animatedquad.h
@@ -24,4 +24,31 @@ namespace windbreeze
             uint32_t currentFrame = 0;
             bool animate = true;
     };
+    /** @addtogroup Render2D
+     *@{
+     *  @class AnimatedQuad
+     *@}
+     ***
+     *  @class AnimatedQuad
+     *  @brief A Quad capable of manipulating its displayed texture by cycling through animation frames.
+     ***
+     *  @fn AnimatedQuad::AnimatedQuad()
+     *  @brief Construct an AnimatedQuad.
+     *  
+     *  Uses the AnimatedQuad::Quad() constructor.
+     ***
+     *  @fn AnimatedQuad::AnimatedQuad(float w, float h)
+     *  @brief Construct an AnimatedQuad with the given size.
+     *  
+     *  Uses the AnimatedQuad::Quad(float w, float h) constructor.
+     *  @param w Width.
+     *  @param h Height.
+     ***
+     *  @fn void AnimatedQuad::addAnimation(AnimationId id, std::weak_ptr<Animation> animation)
+     *  @brief Add an animation.
+     * 
+     *  After an animation is added, the animation can be set and played using AnimatedQuad::setAnimation and AnimatedQuad::playAnimation.
+     *  @param id Id to be used to refer to the animation.
+     *  @param animation Pointer to the animation to add. Pointers to animations can be gotten from the AnimationManager.
+     **/
 }
