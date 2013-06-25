2745fe591ee42c44b8241e1214721372d39d3501
Tobias Widlund <widlundtobias@gmail.com>
2013-06-25 03:11:32 +0200

    opengltexture fully documented

--- include/framework/util/rendering/opengltexture.h
+++ include/framework/util/rendering/opengltexture.h
@@ -6,4 +6,15 @@ namespace windbreeze
     {
         uint32_t glId = 0;
     };
+    /** @addtogroup Render2D
+     *@{
+     *  @struct OpenGLTexture
+     *@}
+     ***
+     *  @struct OpenGLTexture
+     *  @brief Contains an ID of an OpenGL texture to be used with the OpenGL2DBackend.
+     ***
+     *  @var OpenGLTexture::glId
+     *  @brief Numerical ID of the texture.
+     ***/
 }
