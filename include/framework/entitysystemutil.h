157f470cafe4338b6d4f01b9db6b13fca6e91138
Tobias Widlund <widlundtobias@gmail.com>
2013-06-27 14:55:40 +0200

    added entity system util bunch uncludred

---
+++ include/framework/entitysystemutil.h
@@ -0,0 +1,4 @@
+#include <framework/util/entity/alignedentitybackend.h>
+#include <framework/util/entity/basicentitybackend.h>
+#include <framework/util/entity/jsonentityloader.h>
+#include <framework/util/entity/defaultsetters.h>
