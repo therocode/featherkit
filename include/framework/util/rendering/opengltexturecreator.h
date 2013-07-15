d49e9a1f131059d46eb84edcb99f52cb2dc62ead
Tobias Widlund <widlundtobias@gmail.com>
2013-07-15 23:38:34 +0200

    added opengl texture creator

---
+++ include/framework/util/rendering/opengltexturecreator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+#include <framework/util/rendering/opengltexture.h>
+#include <framework/util/rendering/opengl.h>
+
+namespace windbreeze
+{
+    enum ResizeAlgorithm{NEAREST, LINEAR};
+
+    class OpenGLTextureCreator
+    {
+        public:
+            OpenGLTexture createTexture(const unsigned char* pixels, uint32_t width, uint32_t height, ResizeAlgorithm algo) const;
+    };
+}
---
+++ src/util/rendering/opengltexturecreator.cpp
@@ -0,0 +1,31 @@
+#include <framework/util/rendering/opengltexturecreator.h>
+
+namespace windbreeze
+{
+    OpenGLTexture OpenGLTextureCreator::createTexture(const unsigned char* pixels, uint32_t width, uint32_t height, ResizeAlgorithm algo) const
+    {
+        windbreeze::OpenGLTexture texture;
+
+        GLuint id;
+
+        glGenTextures(1, &id);
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+        if(algo == NEAREST)
+        {
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+        }
+        else if(algo == LINEAR)
+        {
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+        }
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        texture.glId = id;
+
+        return texture;
+    }
+}
