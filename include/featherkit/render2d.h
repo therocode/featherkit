b03db2f9d14abeac9840a9b7d40eff7f6be3438d
Tobias Widlund <widlundtobias@gmail.com>
2013-08-02 02:59:00 +0200

    removed tex manager

--- include/featherkit/render2d.h
+++ include/featherkit/render2d.h
@@ -1,6 +1,5 @@
 #include <featherkit/rendering/renderer2d.h>
 #include <featherkit/rendering/repeatedquad.h>
 #include <featherkit/rendering/text.h>
-#include <featherkit/rendering/texturemanager.h>
 #include <featherkit/rendering/tilemap.h>
 #include <featherkit/rendering/animationmanager.h>
