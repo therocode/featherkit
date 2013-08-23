7d1640ea9e468c5b0b00c4c41df6572b97d6e28e
Tobias Widlund <widlundtobias@gmail.com>
2013-08-23 19:39:56 +0200

    yay insertion works!

--- include/featherkit/util/loosentree.h
+++ include/featherkit/util/loosentree.h
@@ -30,35 +30,69 @@ namespace fea
     class LooseNTree
     {
         public:
-            class Position
+            class Vector
             {
                 public:
-                    float operator[](const int32_t i)
+                    Vector()
+                    {
+                    }
+                    Vector(std::initializer_list<float> args)
+                    {
+                        float* start = coords;
+                        for(auto coord : args)
+                        {
+                            *start = coord;
+                            start++;
+                        }
+                    }
+
+                    float& operator[](const int32_t i)
                     {
                         return coords[i];
                     }
-                private:
-                    float coords[Dimensions];
-            };
-            
-            class Size
-            {
-                private:
-                    float operator[](const int32_t i)
+
+                    float operator[](const int32_t i) const
                     {
-                        return widths[i];
+                        return coords[i];
                     }
 
-                    bool operator<=(const Size& other)
+                    bool operator<=(const Vector& other) const
                     {
                         for(uint32_t d = 0; d < Dimensions; d++)
-                            if(widths[d] > other[d])
+                            if(coords[d] > other.coords[d])
                                 return false;
 
                         return true;
                     }
 
-                    float widths[Dimensions];
+                    Vector operator/(const Vector& other) const
+                    {
+                        Vector result;
+                        for(uint32_t d = 0; d < Dimensions; d++)
+                            result[d] = coords[d] / other.coords[d];
+
+                        return result;
+                    }
+
+                    Vector operator/(float divisor) const
+                    {
+                        Vector result;
+                        for(uint32_t d = 0; d < Dimensions; d++)
+                            result[d] = coords[d] / divisor;
+
+                        return result;
+                    }
+
+                    Vector& operator=(const Vector& other)
+                    {
+                        for(uint32_t d = 0; d < Dimensions; d++)
+                            coords[d] = other.coords[d];
+                        return *this;
+                    }
+
+                private:
+
+                    float coords[Dimensions];
             };
 
             struct Node
@@ -72,14 +106,27 @@ namespace fea
                 bool empty;
                 uint32_t children[Pow<2, Dimensions>::value];
                 uint32_t parent;
+
+                void printNode(Node* nodes)
+                {
+                    std::cout << "HEJ!\n";
+
+                    for(uint32_t child = 0; child < pow(2, Dimensions); child++)
+                    {
+                        std::cout << "checking kid number " << child << " and it has index " << children[child] << "\n";
+                        if(children[child] != 0)
+                            nodes[children[child]].printNode(nodes);
+                    }
+                }
             };
 
             struct Entry
             {
+                uint32_t id;
 
             };
 
-            LooseNTree() 
+            LooseNTree(float width, float height) : size({width, height})
             {
                 if(StaticAllocation)
                 {
@@ -94,11 +141,12 @@ namespace fea
                     {
                         for(uint32_t j = 0; j < Pow<2, Dimensions>::value; j++)
                         {
-                            std::cout << "i am id " << i << " and i want a child! I claim " << nextFreeIndex << " as mine!\n";
+                            std::cout << "i am index " << i << " and i want a child! I claim " << nextFreeIndex << " as mine!\n";
+                            nodes[nextFreeIndex].parent = i;
                             nodes[i].children[j] = nextFreeIndex++;
-                            nodes[i].parent = i;
                         }
                     }
+                    nodes[0].printNode(nodes);
                 }
                 else
                 {
@@ -110,31 +158,72 @@ namespace fea
                 std::cout << "Allocated: " << allocatedNodesCount << " used: " << usedNodesCount << " root node id is " << 0 << "\n";
             }
 
-            void Add(uint32_t id, Position pos, Size s)
+            void add(uint32_t id, float x, float y, float width, float height)
+            {
+                add(id, Vector({x, y}), Vector({width, height}));
+            }
+
+            void add(uint32_t id, const Vector& pos, const Vector& s)
             {
                 uint32_t depth;
 
-                Size nextLooseBounds = size;
-                for(depth = 0; depth <= Depth; depth++)
+                Vector nextLooseBounds = size;
+                for(depth = 0; depth < Depth; depth++)
                 {
+                    std::cout << "testing depth " << depth + 1 << " with loose bounds being " << nextLooseBounds[0] << " " << nextLooseBounds[1] << "...\n";
                     if(!(s <= nextLooseBounds))
                     {
+                        std::cout << "it doesn't fit so will use depth " << depth << "\n";
                         break;
                     }
+                    nextLooseBounds = nextLooseBounds / 2.0f;
+                    std::cout << "fits in this depth\n";
                 }
 
-                //coords in the current depth grid = pos / nextLooseBounds 
-            }
+                Vector positionPercent = pos / size;
+                Node& currentNode = nodes[0];
+                uint32_t targetNodeIndex = 0;
 
+                std::cout << "going to find the right node now. it should be in depth " << depth << "\n";
+                for(uint32_t d = 0; d < depth; d++)
+                {
+                    uint32_t childIndex = 0;
+                    std::cout << "on depth " << d << "\n";
+                    for(uint32_t dim = 0; dim < Dimensions; dim++)
+                    {
+                        std::cout << "comparing axis " << dim << "\n";
+                        if(positionPercent[dim] > 0.5f)
+                        {
+                            childIndex += pow(2, dim);
+                            positionPercent[dim] = (positionPercent[dim] - 50.0f) * 2.0f;
+                            std::cout << "it was over the half\n";
+                        }
+                        else
+                        {
+                            positionPercent[dim] = positionPercent[dim] * 2.0f;
+                            std::cout << "it was under the half\n";
+                        }
+                    }
+                    targetNodeIndex = currentNode.children[childIndex];
+                    currentNode = nodes[currentNode.children[childIndex]];
+                }
+                entries.emplace(targetNodeIndex, Entry({id}));
+
+
+                std::cout << "the thing ended up in node index " << targetNodeIndex << "\n";
+                //nodes[0].printNode(nodes);
+            }
+ 
             ~LooseNTree()
             {
                 delete [] nodes;
             }
         private:
-            float size;
+            Vector size;
 
             Node* nodes;
             uint32_t allocatedNodesCount;
             uint32_t usedNodesCount;
+            std::unordered_multimap<uint32_t, Entry> entries;
     };
 }
