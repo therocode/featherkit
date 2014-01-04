8767683779cc7ceff03a2e310527d55aa7f02a1b
Tobias Widlund <widlundtobias@gmail.com>
2014-01-04 23:16:50 +0800

    it is now possible to select if text rendering capabilities should be built

--- include/featherkit/render2d.h
+++ include/featherkit/render2d.h
@@ -1,5 +1,4 @@
 #include <featherkit/rendering/renderer2d.h>
 #include <featherkit/rendering/subrectquad.h>
 #include <featherkit/rendering/repeatedquad.h>
-#include <featherkit/rendering/text.h>
 #include <featherkit/rendering/tilemap.h>
