47d75d6f90f34a107efce478d9160163bb16429e
Tobias Widlund <widlundtobias@gmail.com>
2014-01-21 17:11:49 +0800

    documented font

--- include/featherkit/rendering/font.h
+++ include/featherkit/rendering/font.h
@@ -26,6 +26,43 @@ namespace fea
             std::string fontPath;
             float fontSize;
     };
+    
+    /** @addtogroup Render2D
+     *@{
+     *  @class Font
+     *@}
+     ***
+     *  @class Font
+     *  @brief Font object to use with fea::TextSurface
+     *
+     *  The font object holds font data which is used by the fea::TextSurface class when writing text. Fonts can right now only be loaded from file. The size given to a font object is important since it will determine the resolution and memory foot print of the font. A font with a bigger size will look more smooth, but also take upp more memory and vice versa. 
+     ***
+     *  @fn Font::Font(const std::string& path = "", const float size = 12.0f)
+     *  @brief Construct a font from the given file name and size.
+     *  @param path Path to file.
+     *  @param size Size of the font.
+     ***
+     *  @fn void Font::setPath(const std::string& path)
+     *  @brief Load another file into the font.
+     *  @param path Path to file.
+     ***
+     *  @fn void Font::setSize(const float size)
+     *  @brief Set the size of the font.
+     *  @param size Size to set to.
+     ***
+     *  @fn const Font::std::string& getPath() const
+     *  @brief Get the current path in use.
+     *  @return The path.
+     ***
+     *  @fn float Font::getSize() const
+     *  @brief Get the current size of the font.
+     *  @return The size.
+     ***
+     *  @fn bool Font::operator==(const Font& other) const
+     *  @brief Compare two font instances.
+     *  @param other Font to compare with.
+     *  @return True if the instances are the same.
+     ***/
 }
 
 namespace std
