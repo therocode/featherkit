50f728576cc92601a331468df74f71772fed71d0
Tobias Widlund <widlundtobias@gmail.com>
2014-02-13 19:22:56 +0800

    improved asserts

--- src/util/entity/basictypeadder.cpp
+++ src/util/entity/basictypeadder.cpp
@@ -32,6 +32,7 @@ namespace fea
             factory.addDataType("uint8", [] (const Parameters& params)
             {
                 FEA_ASSERT(params.size() == 1, "Wrong amount of parameters! Expected 1, got " + std::to_string(params.size()) + "!");
+                FEA_ASSERT(params[0][0] != '-', "Default value for type uint8 is not allowed to be negative! Given '" + params[0] + "'.");
                 FEA_ASSERT(static_cast<unsigned long>(static_cast<uint8_t>(std::stoul(params[0]))) == std::stoul(params[0]), "Default value '" + params[0] + "' given to attribute of uint8 is too large!");
 
                 return static_cast<uint8_t>(std::stoul(params[0]));
@@ -48,6 +49,7 @@ namespace fea
             factory.addDataType("uint16", [] (const Parameters& params)
             {
                 FEA_ASSERT(params.size() == 1, "Wrong amount of parameters! Expected 1, got " + std::to_string(params.size()) + "!");
+                FEA_ASSERT(params[0][0] != '-', "Default value for type uint16 is not allowed to be negative! Given '" + params[0] + "'.");
                 FEA_ASSERT(static_cast<unsigned long>(static_cast<uint16_t>(std::stoul(params[0]))) == std::stoul(params[0]), "Default value '" + params[0] + "' given to attribute of uint16 is too large!");
 
                 return static_cast<uint16_t>(std::stoul(params[0]));
@@ -64,6 +66,7 @@ namespace fea
             factory.addDataType("uint32", [] (const Parameters& params)
             {
                 FEA_ASSERT(params.size() == 1, "Wrong amount of parameters! Expected 1, got " + std::to_string(params.size()) + "!");
+                FEA_ASSERT(params[0][0] != '-', "Default value for type uint32 is not allowed to be negative! Given '" + params[0] + "'.");
                 FEA_ASSERT(static_cast<unsigned long>(static_cast<uint32_t>(std::stoul(params[0]))) == std::stoul(params[0]), "Default value '" + params[0] + "' given to attribute of uint32 is too large!");
 
                 return static_cast<uint32_t>(std::stoul(params[0]));
@@ -79,6 +82,7 @@ namespace fea
             factory.addDataType("uint64", [] (const Parameters& params)
             {
                 FEA_ASSERT(params.size() == 1, "Wrong amount of parameters! Expected 1, got " + std::to_string(params.size()) + "!");
+                FEA_ASSERT(params[0][0] != '-', "Default value for type uint64 is not allowed to be negative! Given '" + params[0] + "'.");
 
                 return static_cast<uint64_t>(std::stoull(params[0]));
             });
