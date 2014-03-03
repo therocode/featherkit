178d1de08e79f1829ff40a62b3ec4629db0f4038
Tobias Widlund <widlundtobias@gmail.com>
2014-03-03 15:43:41 +0800

    added listener class

---
+++ include/featherkit/audio/listener.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <featherkit/audio/vec3f.h>
+
+namespace fea
+{
+    class Listener
+    {
+        public:
+            Listener();
+            void setGain(float gain);
+            float getGain() const;
+            void setPosition(const Vec3F& position);
+            const Vec3F& getPosition() const;
+            void setVelocity(const Vec3F& velocity);
+            const Vec3F& getVelocity() const;
+            void setOrientation(const Vec3F& at, const Vec3F& up);
+            const Vec3F& getOrientationAt() const;
+            const Vec3F& getOrientationUp() const;
+        private:
+            float mGain;
+            Vec3F mPosition;
+            Vec3F mVelocity;
+            Vec3F mAt;
+            Vec3F mUp;
+    };
+}
---
+++ src/audio/listener.cpp
@@ -0,0 +1,62 @@
+#include <featherkit/audio/listener.h>
+#include <featherkit/assert.h>
+#include <string>
+
+namespace fea
+{
+    Listener::Listener() :
+        mGain(1.0f),
+        mPosition({0.0f, 0.0f, 0.0f}),
+        mVelocity({0.0f, 0.0f, 0.0f}),
+        mAt({0.0f, 0.0f, -1.0f}),
+        mUp({0.0f, 1.0f, 0.0f})
+    {
+    }
+
+    void Listener::setGain(float gain)
+    {
+        FEA_ASSERT(gain > 0.0f, "Trying to set gain to zero or below! Given " + std::to_string(gain));
+        mGain = gain;
+    }
+
+    float Listener::getGain() const
+    {
+        return mGain;
+    }
+
+    void Listener::setPosition(const Vec3F& position)
+    {
+        mPosition = position;
+    }
+
+    const Vec3F& Listener::getPosition() const
+    {
+        return mPosition;
+    }
+
+    void Listener::setVelocity(const Vec3F& velocity)
+    {
+        mVelocity = velocity;
+    }
+
+    const Vec3F& Listener::getVelocity() const
+    {
+        return mVelocity;
+    }
+
+    void Listener::setOrientation(const Vec3F& at, const Vec3F& up)
+    {
+        mAt = at;
+        mUp = up;
+    }
+
+    const Vec3F& Listener::getOrientationAt() const
+    {
+        return mAt;
+    }
+
+    const Vec3F& Listener::getOrientationUp() const
+    {
+        return mUp;
+    }
+}
