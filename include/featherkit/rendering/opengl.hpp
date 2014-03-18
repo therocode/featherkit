46717eae330c868ce2da35beb0f448a84a72a775
Tobias Widlund <widlundtobias@gmail.com>
2014-03-18 10:13:46 +0800

    changed gl includes for mac

--- include/featherkit/rendering/opengl.hpp
+++ include/featherkit/rendering/opengl.hpp
@@ -1,4 +1,9 @@
-#define GLEW_STATIC
+#if defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh)
+#include <OpenGL/glew.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
