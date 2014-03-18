1f4c444d33b1bc8f1ff9d7729aeab39c7c6188e6
Tobias Widlund <widlundtobias@gmail.com>
2014-03-18 10:16:14 +0800

    now then?

--- include/featherkit/rendering/opengl.hpp
+++ include/featherkit/rendering/opengl.hpp
@@ -1,9 +1,8 @@
+#include <GL/glew.h>
 #if defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh)
-#include <OpenGL/glew.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
-#include <GL/glew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #endif
