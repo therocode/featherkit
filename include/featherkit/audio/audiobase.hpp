ff2754208aeffaf36335ea86bdedcc325ffdcd3f
Tobias Widlund <widlundtobias@gmail.com>
2014-03-25 15:55:49 +0800

    started on audibase documentation

--- include/featherkit/audio/audiobase.hpp
+++ include/featherkit/audio/audiobase.hpp
@@ -49,4 +49,75 @@ namespace fea
             std::set<size_t> mEffectSends;
             const AudioFilter* mFilter;
     };
+    /** @addtogroup Audio
+     *@{
+     *  @class AudioBase
+     *@}
+     ***
+     *  @class AudioBase
+     *  @brief Base class for playable audio
+     *
+     *  Classes inheriting this class will carry many properties of playable audio. These properties are:
+     *      - Position - 3D Position in the audio scene.
+     *      - Velocity - 3D Velocity. This can be used to create doppler effects.
+     *      - Pitch - Modifier for the frequency.
+     *      - Gain - Amplification of the sound, or in other words volume.
+     *      - Attenuation Factor - Affects how far away the sound can be heard.
+     *      - Looping - Toggle if the sound should be played repeatedly or not.
+     *      - Relative - A relative audio is played with its position relative to the listener.
+     *      - Play Offset - Decides where in the audio data the playback begins.
+     *
+     *  Check the setters of the properties to see what input they expect.
+     ***
+     *  @fn AudioBase::AudioBase()
+     *  @brief Construct an AudioBase. 
+     ***
+     *  @fn void AudioBase::setPosition(const Vec3F& position)
+     ***
+     *  @fn const AudioBase::Vec3F& getPosition() const
+     ***
+     *  @fn void AudioBase::setVelocity(const Vec3F& velocity)
+     ***
+     *  @fn const AudioBase::Vec3F& getVelocity() const
+     ***
+     *  @fn void AudioBase::setPitch(float pitch)
+     ***
+     *  @fn float AudioBase::getPitch() const
+     ***
+     *  @fn void AudioBase::setGain(float pitch)
+     ***
+     *  @fn float AudioBase::getGain() const
+     ***
+     *  @fn void AudioBase::setAttenuationFactor(float attenuationFactor)
+     ***
+     *  @fn float AudioBase::getAttenuationFactor() const
+     ***
+     *  @fn void AudioBase::setAttenuationDistance(float attenuationDistance)
+     ***
+     *  @fn float AudioBase::getAttenuationDistance() const
+     ***
+     *  @fn void AudioBase::setLooping(bool loop)
+     ***
+     *  @fn bool AudioBase::isLooping() const
+     ***
+     *  @fn void AudioBase::setRelative(bool relative)
+     ***
+     *  @fn bool AudioBase::isRelative()
+     ***
+     *  @fn void AudioBase::setPlayOffset(std::chrono::milliseconds timePoint)
+     ***
+     *  @fn std::chrono::milliseconds AudioBase::getPlayOffset() const
+     ***
+     *  @fn void AudioBase::addEffectSend(size_t slot)
+     ***
+     *  @fn const AudioBase::std::set<size_t> getEffectSends() const
+     ***
+     *  @fn void AudioBase::setFilter(const AudioFilter& filter)
+     ***
+     *  @fn const AudioBase::AudioFilter& getFilter() const
+     ***
+     *  @fn bool AudioBase::hasFilter() const
+     ***
+     *  @fn void AudioBase::clearFilter()
+     **/
 }
