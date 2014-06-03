d5e9b352be28017318cec386b2e84f2880fe4121
Jonas Platte <mail@jonasplatte.de>
2014-06-03 14:33:54 +0200

    Updated JsonEntityLoader to use the new entity format, simplified JsonActionIOHandler

--- src/entity/jsonentityloader.cpp
+++ src/entity/jsonentityloader.cpp
@@ -1,10 +1,19 @@
 #include <fea/entity/jsonentityloader.hpp>
 #include <fstream>
 #include <json/reader.h>
-#include <json/value.h>
 
 namespace fea
 {
+    std::unordered_map<std::string, std::string> JsonEntityLoader::jsonObjToStringMap(const Json::Value& obj)
+    {
+        std::unordered_map<std::string, std::string> result;
+
+        for(const std::string& attribute : obj.getMemberNames())
+            result.emplace(attribute, obj[attribute].asString());
+
+        return result;
+    }
+
     std::vector<std::pair<std::string, EntityTemplate>> JsonEntityLoader::loadEntityTemplates(const std::string& path)
     {
         std::ifstream file(path);
@@ -20,36 +29,30 @@ namespace fea
 
         std::vector<std::pair<std::string, EntityTemplate>> result;
 
-        Json::Value entities = root["entities"];
+        const Json::Value& entities = root["entities"];
         if(entities.isArray())
         {
             for(const Json::Value& entity : entities)
             {
                 EntityTemplate entityTemplate;
 
-                std::string entityName = entity.getMemberNames().front();
-                Json::Value entityData = entity[entityName];
+                std::string entityName = entity["name"].asString();
 
-                Json::Value parents = entityData["inherits"];
+                const Json::Value& parents = entity["inherits"];
                 if(parents.isArray())
                 {
-                    for(Json::Value& parent : parents)
+                    for(const Json::Value& parent : parents)
                         entityTemplate.mInherits.push_back(parent.asString());
                 }
 
-                Json::Value attributes = entityData["attributes"];
-                if(attributes.isObject())
-                {
-                    for(const std::string& a : attributes.getMemberNames())
-                        entityTemplate.mAttributes.emplace(a, attributes[a].asString());
-                }
+                entityTemplate.mAttributes = jsonObjToStringMap(entity["attributes"]);
 
                 result.emplace_back(entityName, entityTemplate);
             }
         }
         return result;
     }
-    
+
     std::unordered_map<std::string, std::string> JsonEntityLoader::loadEntityAttributes(const std::string& path)
     {
         std::ifstream file(path);
@@ -61,12 +64,8 @@ namespace fea
         Json::Reader reader;
 
         reader.parse(file, root, false);
+        file.close();
 
-        std::unordered_map<std::string, std::string> result;
-
-        for(const std::string& attribute : root.getMemberNames())
-            result.emplace(attribute, root[attribute].asString());
-
-        return result;
+        return jsonObjToStringMap(root);
     }
 }
